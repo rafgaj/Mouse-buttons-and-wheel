46aa5af9e50f784eb515dda83ce24bf3e27d8e48
Rafał Gajek <rafalg78@gmail.com>
2023-03-02 17:55:14 +0100

    Initial release

---
+++ Left/code.py
@@ -0,0 +1,120 @@
+import time
+import board
+import digitalio
+import supervisor
+from adafruit_hid.mouse import Mouse
+from adafruit_debouncer import Button
+from digitalio import DigitalInOut, Direction, Pull
+from seeed_xiao_nrf52840 import Battery
+
+# imports needed for bluetooth
+import adafruit_ble
+from adafruit_ble.advertising import Advertisement
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.standard.hid import HIDService
+from adafruit_ble.services.standard.device_info import DeviceInfoService
+
+# set LEDs
+blue_led = DigitalInOut(board.LED_BLUE)
+blue_led.direction = Direction.OUTPUT
+green_led = DigitalInOut(board.LED_GREEN)
+green_led.direction = Direction.OUTPUT
+red_led = DigitalInOut(board.LED_RED)
+red_led.direction = Direction.OUTPUT
+blue_led.value = True  # turn off LED
+green_led.value = True  # turn off LED
+red_led.value = True  # turn off LED
+
+# Battery set
+battery = Battery()
+print(f"Charge status (True-full chargerd, False-otherwise): {battery.charge_status}")
+print(f"Voltage: {battery.voltage}V")
+battery.charge_current = battery.CHARGE_100MA  # Setting charge current to high
+print(f"Charge current (0-50mA, 1-100mA): {battery.charge_current}")
+
+# setup bluetooth
+hid = HIDService()
+device_info = DeviceInfoService(software_revision=adafruit_ble.__version__)
+advertisement = ProvideServicesAdvertisement(hid)
+advertisement.appearance = 961
+scan_response = Advertisement()
+scan_response.complete_name = "Left Mouse Ring"
+ble = adafruit_ble.BLERadio()
+
+# set buttons
+left_BTN_input = digitalio.DigitalInOut(board.D10)
+left_BTN_input.switch_to_input(digitalio.Pull.UP)
+left_BTN = Button(left_BTN_input)
+right_BTN_input = digitalio.DigitalInOut(board.D7)
+right_BTN_input.switch_to_input(digitalio.Pull.UP)
+right_BTN = Button(right_BTN_input)
+scrollup_BTN = digitalio.DigitalInOut(board.D9)
+scrollup_BTN.direction = Direction.INPUT
+scrollup_BTN.pull = Pull.UP
+scrolldown_BTN = digitalio.DigitalInOut(board.D8)
+scrolldown_BTN.direction = Direction.INPUT
+scrolldown_BTN.pull = Pull.UP
+
+# set mouse
+mouse = Mouse(hid.devices)
+
+def battery_status():
+    if supervisor.runtime.serial_connected:
+        if battery.charge_status is True:
+            red_led.value = False
+        else:
+            red_led.value = True
+            time.sleep(0.5)
+            red_led.value = False
+            time.sleep(0.5)
+
+while True:
+    ble.start_advertising(advertisement, scan_response)
+    print("Advertising...")
+    while not ble.connected:
+        print("Connecting...")
+        blue_led.value = False
+        time.sleep(0.5)
+        blue_led.value = True
+        time.sleep(0.5)
+        pass
+    # Now we're connected
+    ble.stop_advertising()
+    print(f"Connected {ble.connections}")
+    # print(ble.connections)
+
+    while ble.connected:
+
+        blue_led.value = False  # turn on BLUE LED
+        green_led.value = True  # reset LED status
+        red_led.value = True  # reset LED status
+
+        # read buttons state
+        left_BTN.update()
+        right_BTN.update()
+
+        if left_BTN.pressed:
+            mouse.click(Mouse.LEFT_BUTTON)
+            print("Left Button is pressed")
+            blue_led.value = True
+            green_led.value = False
+        elif right_BTN.pressed:
+            mouse.click(Mouse.RIGHT_BUTTON)
+            print("Right Button is pressed")
+            blue_led.value = True
+            green_led.value = False
+        elif not scrollup_BTN.value:
+            mouse.move(wheel=1)
+            print("Up Button is pressed")
+            blue_led.value = True
+            green_led.value = False
+        elif not scrolldown_BTN.value:
+            mouse.move(wheel=-1)
+            print("Down Button is pressed")
+            blue_led.value = True
+            green_led.value = False
+        # else:
+            # print("Buttons are not pressed")
+        # battery_status()
+        time.sleep(0.1)  # sleep for debounce
+    ble.start_advertising(advertisement)
--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # Mouse-buttons-and-wheel
- Left and Right Rings with buttons emulating mouse clicks and wheel
 ## Description
+![Rings](/images/rings.png)
+
 HID controller with 4 buttons. There are: left mouse click, right mouse click, wheel up and wheel down as buttons. Built on the basis of the "Seeed Studio XIAO nRF52840" microcontroller and programmed in CircuitPython. Cotroller is connecting with PC by Bluetooth.
 
 The code was created for the need of easy mouse control with VR headset. Microcontroller is a core part of Ring made of according the [instruction](https://www.instructables.com/).
 
 ## Pre-work with the microcontroller
+1. Download CircuitPython .uf2 dedicated loader from [here](https://circuitpython.org/board/Seeed_XIAO_nRF52840_Sense/).
+2. Installing CircuitPython on the microcontroller according the [instruction](https://learn.adafruit.com/welcome-to-circuitpython).
+3. Add CircuitPython library files. Can be download from the [official libraries](https://circuitpython.org/libraries) on the CircuitPython website. Required libriaries are:
+    - /adafruit_ble
+    - /adafruit_ble_adafruit
+    - /adafruit_bluefruit_connect
+    - /adafruit_bus_device
+    - /adafruit_hid
+    - /adafruit_lsm6ds
+    - /adafruit_register
+    - adafruit_debouncer.mpy
+    - adafruit_ticks.mpy
+    - simpleio.mpy
+
+    Unzip downloaded library file and copy required files onto the CIRCUITPY drive into the /lib directory.
+4. To have control with battery charging current download *[seeed_xiao_nrf52840.py](https://pypi.org/project/circuitpython-seeed-xiao-nrf52840/)* and place directly on the CIRCUITPY drive.
+
+## Programming the microcontroller
+Copy the files: *code.py* onto the CIRCUITPY directly. Respectively for the left and right Ring. Reconnect device. Will be visible for Bluetooth and ready to connect.
