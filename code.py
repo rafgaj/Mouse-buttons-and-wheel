9c5e80ba54fc38902a5214e769137eedd2995edf
Greg Retkowski <greg@rage.net>
2024-05-23 18:04:44 -0700

    improvements to responsiveness, charge indicator
    * Removed 'sleeps' which block the rest of the code from
    working; this refactor makes buttons more responsive and
      improve their predictability/reliability.work more reliably
    * Added LED indication of battery levels, charging/80% is green
       yellow up to 30% and red below that.
    * reorganized code, cleanups and comments

--- code.py
+++ code.py
@@ -16,23 +16,42 @@ from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.standard.hid import HIDService
 from adafruit_ble.services.standard.device_info import DeviceInfoService
 
+
+# config defaults - we load hand specific configs from the config.py
+# file. Any customizations can be made in config.py so that no
+# edits are required to code.py when the code is updated.
 from config import config as hand_config
-
-# Logging Levels
-LLVL={'debug': 0, 'info': 1, 'warn': 2, 'error':3}
-
-# config defaults
 config = {
     'charge_current': Battery.CHARGE_50MA,
     'log_level': 'info',
     'log_to_disk': True,
+    'blink_interval': 20000, # lower to blink status more frequently
+    # for button debounce
+    'debounce_sleep': 0.15,
     # Scroll settings
     'sp_initial': 0.2,
-    'sp_accel': 0.02,
-    'sp_max': 0.03,
+    'sp_accel': 0.015,
+    'sp_max': 0.01,
 }
 config.update(hand_config)
 
+
+# Instead of sleeps, which block any other operations,  we get the
+# timestamp for some time in the future, and do checks for passing that
+# time to take the required follow-on actions - ex turning off LEDs
+# after turning them on, handling switch debouncing, scrollwheel
+# acceleration
+LEDOFF_TIME = None
+DEBOUNCE_TIME = None
+def get_delay_time(seconds):
+    return time.monotonic_ns() + (seconds * 10**9)
+
+
+###########################
+# Code for logging messages 
+###########################
+# Logging Levels
+LLVL={'debug': 0, 'info': 1, 'warn': 2, 'error':3}
 # Set up logfile - rotate old ones.
 logfile_handle = None
 if config['log_to_disk']:
@@ -68,6 +87,11 @@ backtrace = supervisor.get_previous_traceback()
 if backtrace is not None:
     log('error',"Previous run crashed.. backtrace follows...")
     log('error',backtrace)
+
+
+######################
+# Set up initial state
+######################
 
 # set LEDs
 blue_led = DigitalInOut(board.LED_BLUE)
@@ -112,28 +136,72 @@ scrolldown_BTN.pull = Pull.UP
 
 # set mouse
 mouse = Mouse(hid.devices)
-i = 1
+# LED blink interval counter
+i = -1
+scroll_sleep = config['sp_initial']
+
+
+##########################
+# Battery related routines
+##########################
+def get_batt_percent(volts):
+    # Returns battery capacity percent as an integer
+    # from 0 to 100.
+    batt_table = {
+        4.2: 100,
+        4.13: 90,
+        4.06: 80,
+        3.99: 70,
+        3.92: 60,
+        3.85: 50,
+        3.78: 40,
+        3.71: 30,
+        3.64: 20,
+        3.57: 10,
+        3.5:  0
+    }
+    for k in sorted(batt_table.keys(), reverse = True):
+        v = batt_table[k]
+        if volts < k:
+            # fallthrough to next higher batt level
+            continue
+        percent = v
+        # TODO - average out between the two keys to get
+        # more granular than just 10% increments
+        break
+    return percent
 
 def battery_leds():
-    # set green/orange/red LED based on battery state.
+    # set green/orange/red LED based on battery state/level.
     # 3.7V lithium ion battery can be considered dead (completely discharged) at a voltage of 3.4V
     volts = battery.voltage
-    if volts > 3.7:
-        green_led.value = False  # turn off LED
-    elif volts > 3.5:
+    percent = get_batt_percent(volts)
+
+    charge_status = battery.charge_status
+    log('info',f"Voltage: {battery.voltage}V Pct: {percent}% Charged: {charge_status}")
+    #if volts > 3.7:
+    if charge_status:
+        green_led.value = False  # turn on LED
+    elif percent > 79:
+        green_led.value = False
+    elif percent > 29:
         green_led.value = False
         red_led.value = False
     else:
-        # below 3.5
+        # below 3.5 / 30%
         red_led.value = False
 
+
+# Turns off all LED's
 def leds_off():
     blue_led.value = True  # reset LED status
     green_led.value = True  # reset LED status
     red_led.value = True  # reset LED status
 
-scroll_sleep = config['sp_initial']
 
+###########
+# MAIN LOOP
+###########
 while True:
     if not ble.connected:
         ble.start_advertising(advertisement, scan_response)
@@ -151,58 +219,71 @@ while True:
 
     while ble.connected:
 
-        if i == 80000:
+        # Perform status LED blinks for bluetooth/voltage
+        if i == config['blink_interval'] * 2:
             blue_led.value = False
-            time.sleep(0.1)
-            i = 0
-        elif i == 40000:
+            LEDOFF_TIME = get_delay_time(0.1)
+            i = -1
+        elif i == config['blink_interval']:
             battery_leds()
-            time.sleep(0.1)
-            i = i+1
-        else:
+            LEDOFF_TIME = get_delay_time(0.1)
+        elif i % 1000 == 0:
+            log('debug',f"LEDOFF {LEDOFF_TIME} MONO {time.monotonic_ns()}")
+        elif LEDOFF_TIME is not None and LEDOFF_TIME < time.monotonic_ns():
+            log('debug',f"LightsOut {LEDOFF_TIME} MONO {time.monotonic_ns()}")
+            LEDOFF_TIME = None
             leds_off()
-            i = i+1
+            #i = i+1
+        i = i+1
 
+        # Handle button clicks
         if left_BTN.value is False:
+            if DEBOUNCE_TIME is not None and DEBOUNCE_TIME > time.monotonic_ns():
+                continue
+            DEBOUNCE_TIME = get_delay_time(config['debounce_sleep'])
             # mouse.click(Mouse.LEFT_BUTTON)
             mouse.press(Mouse.LEFT_BUTTON)
             while left_BTN.value is False:
                 pass
             mouse.release(Mouse.LEFT_BUTTON)
             log('info',"Left Button is pressed")
-            # blue_led.value = True
-            # green_led.value = False
-            # time.sleep(0.2)  # sleep for debounce
+
         elif right_BTN.value is False:
-            # mouse.click(Mouse.RIGHT_BUTTON)
+            if DEBOUNCE_TIME is not None and DEBOUNCE_TIME > time.monotonic_ns():
+                continue
+            DEBOUNCE_TIME = get_delay_time(config['debounce_sleep'])
+
             mouse.press(Mouse.RIGHT_BUTTON)
             while right_BTN.value is False:
                 pass
             mouse.release(Mouse.RIGHT_BUTTON)
             log('info',"Right Button is pressed")
-            # blue_led.value = True
-            # green_led.value = False
-            # time.sleep(0.2)  # sleep for debounce
+
         elif not scrollup_BTN.value:
+            if DEBOUNCE_TIME is not None and DEBOUNCE_TIME > time.monotonic_ns():
+                continue
+            DEBOUNCE_TIME = get_delay_time(scroll_sleep)
+            scroll_sleep -= config['sp_accel']
+            if scroll_sleep < config['sp_max']:
+                scroll_sleep = config['sp_max']
+            DEBOUNCE_TIME = get_delay_time(scroll_sleep)
             mouse.move(wheel=1)
             log('info',"Up Button is pressed")
-            # blue_led.value = True
-            # green_led.value = False
-            time.sleep(scroll_sleep)  # sleep for debounce
-            if scroll_sleep > config['sp_max']:
-                scroll_sleep -= config['sp_accel']
+
         elif not scrolldown_BTN.value:
+            if DEBOUNCE_TIME is not None and DEBOUNCE_TIME > time.monotonic_ns():
+                continue
+            DEBOUNCE_TIME = get_delay_time(scroll_sleep)
+            scroll_sleep -= config['sp_accel']
+            if scroll_sleep < config['sp_max']:
+                scroll_sleep = config['sp_max']
+            DEBOUNCE_TIME = get_delay_time(scroll_sleep)
             mouse.move(wheel=-1)
             log('info',"Down Button is pressed")
-            # blue_led.value = True
-            # green_led.value = False
-            time.sleep(scroll_sleep)  # sleep for debounce
-            if scroll_sleep > config['sp_max']:
-                scroll_sleep -= config['sp_accel']
+
         else:
             if scroll_sleep != config['sp_initial']:
                 log('info',"scroll_sleep reset")
                 scroll_sleep = config['sp_initial']
-    log('info','Not Connected (lost connection)')
-    # ble.start_advertising(advertisement)
 
+    log('info','Not Connected (lost connection)')
